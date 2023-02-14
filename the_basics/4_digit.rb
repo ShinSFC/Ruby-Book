b37a85effa0a7320a4b41c427d20fadfd1331567
ShinpeiH <dctechsf@gmail.com>
2023-02-13 19:09:45 -0800

    1st commit from Ruby book

---
+++ the_basics/4_digit.rb
@@ -0,0 +1,9 @@
+thousands = 5378 / 1000
+hundreds = 5378 % 1000 / 100
+tens = 5378 % 100 / 10
+ones = 5378 % 10 
+
+puts "thousands: #{thousands}"
+puts "hundreds: #{hundreds}"
+puts "tens: #{tens}"
+puts "ones: #{ones}"
---
+++ the_basics/movie_years.rb
@@ -0,0 +1,2 @@
+movie_years = {A: 1975, B: 2004, C: 2013, D: 2001, E: 1981 }
+movie_years.each { |movie, year| puts year }
