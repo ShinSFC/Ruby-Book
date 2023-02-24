de4d880e803d0d494c75b2cde4a2a05f6292367b
ShinpeiH <dctechsf@gmail.com>
2023-02-23 16:23:36 -0800

    added some flow control

---
+++ example_code.rb
@@ -0,0 +1,40 @@
+# case_statement.rb
+
+a = 5
+
+case a
+when 5
+  puts "a is 5"
+when 6
+  puts "a is 6"
+else
+  puts "a is neither 5, nor 6"
+end
+
+# case_statement.rb <-- refactored (save result of a case statement as a variable)
+
+a = 5
+
+answer = case a
+  when 5
+    "a is 5"
+  when 6
+    "a is 6"
+  else
+    "a is neither 5, nor 6"
+  end
+
+puts answer
+
+# case_with_no_arg_statement.rb
+
+a = 5
+
+case
+when a == 5
+  puts "a is 5"
+when a == 6
+  puts "a is 6"
+else
+  puts "a is neither 5, nor 6"
+end
---
+++ flow_control/exercises_flow.rb
@@ -0,0 +1,12 @@
+def method(string)
+  count = gets.chomp.length
+  string = gets.chomp.upcase
+  if count > 10 
+    puts string.upcase
+  else
+    puts srting
+  end
+end    
+
+    
+method("hello world")
