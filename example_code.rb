86d30eed978a628b8be91893387913107c1e6734
ShinpeiH <dctechsf@gmail.com>
2023-03-10 20:12:43 -0800

    continuing more stuff

--- example_code.rb
+++ example_code.rb
@@ -1,3 +1,38 @@
+# conditional.rb
+
+puts "Put in a number"
+a = gets.chomp.to_i
+
+if a == 3
+  puts "a is 3"
+elsif a == 4
+  puts "a is 4"
+else
+  puts "a is neither 3, nor 4"
+end
+
+# Example 1
+if x == 3
+  puts "x is 3"
+end
+
+# Example 2
+if x == 3
+  puts "x is 3"
+elsif x == 4
+  puts "x is 4"
+end
+
+# Example 3
+if x == 3
+  puts "x is 3"
+else
+  puts "x is NOT 3"
+end
+
+# Example 4: must use "then" keyword when using 1-line syntax
+if x == 3 then puts "x is 3" end
+
 # case_statement.rb
 
 a = 5
@@ -6,6 +41,18 @@ case a
 when 5
   puts "a is 5"
 when 6
+  puts "a is 6"
+else
+  puts "a is neither 5, nor 6"
+end
+
+# if_statement.rb
+
+a = 5
+
+if a == 5
+  puts "a is 5"
+elsif a == 6
   puts "a is 6"
 else
   puts "a is neither 5, nor 6"
