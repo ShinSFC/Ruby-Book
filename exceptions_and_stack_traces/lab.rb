90f9521e61ee73d6da8b106e7eccaf11537c9577
ShinpeiH <dctechsf@gmail.com>
2023-03-12 23:44:03 -0700

    more stuff exercises

---
+++ exceptions_and_stack_traces/lab.rb
@@ -0,0 +1,10 @@
+array = ["laboratory", "experiment", "Pans Labyrinth", "elaborate", "polar bear"]
+
+array.each do |x|
+  if x =~ /lab/
+    puts x
+  else
+   x
+  end
+end
+
--- more_stuff/inline_exception_example.rb
+++ more_stuff/inline_exception_example.rb
@@ -1,1 +1,4 @@
 zero = 0
+puts "Before each call"
+zero.each { |element| puts } rescue puts "Can't do that!"
+puts "After each call"
